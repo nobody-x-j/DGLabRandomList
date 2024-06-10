6338e8095a4d7c76ac9d727528492dada26a4101
nobody-x-j <nobody.x.j.90@gmail.com>
2024-06-10 02:48:53 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,167 @@
 # DGLabRandomList
 郊狼好玩喵~ 键盘按键触发式随机播放列表生成器~
+----
+
+可以根据按键输入随机生成播放列表曲目，可随机的内容有：通道，波形（需要预先设置波形列表，支持DIY），强度，时间
+
+因为这种玩法大家喜欢的波形/道具都因人而异，所以这里以A通道环，B通道塞为蓝本，有需求的可以自行修改代码以匹配自己的玩法。
+
+默认按键：q：低强度随机 
+         w：中强度随机 
+         e：中高强度随机 
+         r：高强度随机 
+         t：最后冲刺 
+         o或者回车键：30分钟项目，每五分钟提高一次随机等级
+         p或者小键盘+号键：60分钟项目，每400秒提高一次随机等级
+
+本程序必须在电脑上运行，你至少需要一台Windows系统的电脑，其他系统没有测试，理论上linux应该可以用
+
+从零开始的安装教程见这里 [DGLabMouseListener2](https://github.com/nobody-x-j/DGLabMouseListener2)
+
+安装完毕之后只需要执行以下步骤：
+
+step 0：使用vscode下载代码
+
+1. 打开miniconda3（windows 10用户可以在开始菜单栏边上搜索miniconda3，然后点击打开），在弹出的Miniconda3框中输入 code，也可以在开始菜单栏边上搜索vscode
+
+```shell
+code
+```
+
+![image](https://github.com/nobody-x-j/images/blob/main/code.png)
+
+之后会弹出以下界面：
+
+![image](https://github.com/nobody-x-j/images/blob/main/vscode_starting.png)
+
+2. 选择 打开文件夹 -> 打开或者新建一个你想要存代码的文件夹
+
+![image](https://github.com/nobody-x-j/images/blob/main/vscode_open_folder.png)
+
+打开后应该是这样：
+
+![image](https://github.com/nobody-x-j/images/blob/main/vscode_folder_opened.png)
+
+3. 在打开的界面选择 "Terminal" -> "New Terminal" （英文界面） /  “终端” -> “新建终端” (中文界面) 如果看不到 "Terminal" / “终端” ，请把窗口全屏
+
+![image](https://github.com/nobody-x-j/images/blob/main/vscode_new_terminal.png)
+
+打开后应该是这样：
+
+![image](https://github.com/nobody-x-j/images/blob/main/vscode_terminal_opened.png)
+
+4. 在终端输入 git clone https://github.com/nobody-x-j/DGLabMouseListener2.git 从而复制本代码到之前选择的文件夹
+
+```shell
+git clone https://github.com/nobody-x-j/DGLabMouseListener2.git
+```
+
+![image](https://github.com/nobody-x-j/images/blob/main/vscode_terminal_clone.png)
+
+按回车确认后终端应该会有如下信息，说明你成功将代码存入你的电脑
+
+![image](https://github.com/nobody-x-j/images/blob/main/vscode_terminal_clone_done.png)
+
+5. 在vscode窗口，按"ctrl + shift + p"(三个键同时按)，在跳出的窗口选择 “Python: Select Interpreter” -> 找到刚刚用miniconda3创建的虚拟环境venv -> "Terminal" -> "New Terminal" （英文界面） /  “终端” -> “新建终端” (中文界面) 此时新的终端的命令输入处最左侧出现（venv）则代表成功启动虚拟环境，否则请重启vscode，再 "Terminal" -> "New Terminal" （英文界面） /  “终端” -> “新建终端” (中文界面)， 若还是失败，请试着在miniconda3界面输入code启动vscode
+
+![image](https://github.com/nobody-x-j/images/blob/main/vscode_ctrl_shift_p.png)
+
+![image](https://github.com/nobody-x-j/images/blob/main/vscode_select_interpreter.png)
+
+成功后的终端：
+
+![image](https://github.com/nobody-x-j/images/blob/main/vscode_venv_success.png)
+
+如果反复测试都不成功，尝试在终端输入：
+
+```shell
+conda activate venv
+```
+
+![image](https://github.com/nobody-x-j/images/blob/main/vscode_venv_manual_activate.png)
+
+如果还不成功，说明之前的步骤操作有误，请删除miniconda3以及vscode，从step 0 重新开始
+
+step 1: 安装以及设置
+
+以下步骤必须确认终端的命令输入处最左侧出现（venv）方可进行
+
+1. 在终端输入 cd DGLabMouseListener2 切换到目标文件夹
+
+```shell
+cd DGLabMouseListener2
+```
+
+![image](https://github.com/nobody-x-j/images/blob/main/vscode_cd.png)
+
+按下回车后会看到：
+
+![image](https://github.com/nobody-x-j/images/blob/main/vscode_cd_finish.png)
+
+2. 在终端输入 pip install requests websockets loguru pynput 来安装必要的前置程序包 （在DGLabMouseListener2执行过这一步的可以跳过）
+
+```shell
+pip install requests websockets loguru pynput
+```
+
+![image](https://github.com/nobody-x-j/images/blob/main/vscode_pip_install.png)
+
+成功安装应该是这样的：
+
+![image](https://github.com/nobody-x-j/images/blob/main/vscode_pip_install_success.png)
+
+3. 在vscode界面的最左端（如图）
+
+![image](https://github.com/nobody-x-j/images/blob/main/vscode_config.png)
+
+选中config.py，将ws 里面的内容根据OTC控制器显示的内容自行修改（注：OTC控制器是本程序需要配套的手机app，[点此下载](https://github.com/open-toys-controller/open-DGLAB-controller/releases/latest/download/app-release.apk) [OTC控制器官网](https://github.com/open-toys-controller/open-DGLAB-controller/releases/tag/V1.2.0)）
+
+![image](https://github.com/lxyddice/DGLabMouseListener/assets/95132858/7f1879b3-bc43-4e10-b46d-3b0f3319c23e)
+
+4. 链接设备，开启娱乐模式后，在终端输入 python main.py 即可启动
+
+```shell
+python main.py
+```
+
+点击鼠标就能看到手机app上显示相应通道有输出了
+
+step 2: 再次开始游戏：
+
+安装之后，再次开始游戏，只需要打开miniconda3，输入 code
+
+```shell
+code
+```
+
+一般默认打开上次的文件夹，如果没有，请选择打开文件夹，选取程序所在文件夹，打开终端，输入：
+
+```shell
+cd DGLabMouseListener2
+```
+
+然后输入
+
+```shell
+python main.py
+```
+
+step 3: 一些自定义选项：
+
+在config.py文件中，有自定义项目，可阅读注释自行修改
+
+在DIY_patterns.py文件中，可以自定义波形，具体格式请参考DIY_patterns.py文件中的注释
+
+需要改变玩法可以自行阅读并修改代码，提示：main.py 42行开始是按键触发相关内容，主要改这里
+
+
+
+## 题外话
+
+发现好多人求控但是没人控，就想着让随机数来控制好了喵~
+
+搜了一圈没人写这个玩法，那就我试试吧（） 写的很烂，求教
+
+写的时候其实也开着>_< 被电的很爽（
+
+## 请注意身体，不要玩过头了喵
---
+++ main.py
@@ -0,0 +1,343 @@
+import asyncio
+import json
+import time
+import random
+import websockets
+from loguru import logger
+import sys
+from pynput import keyboard
+import tracemalloc
+from DIY_patterns import DIY_patterns
+from config import config
+
+tracemalloc.start()
+
+class KeyListener:
+    def __init__(self, loop, caoFanNiController, PlayList):
+        self.loop = loop
+        self.caoFanNiController = caoFanNiController
+        self.listener = keyboard.Listener(on_press=self.on_press)
+        self.listener.start()
+        self.level = config["initial_states"]["keyboard_level"]
+        self.tick = config["initial_states"]["keyboard_tick"]
+        self.soft_cap = config["soft_cap"]["keyboard"]
+        self.PlayList = PlayList
+        self.start_time = time.time()
+        self.current_time = time.time() - self.start_time
+        self.RandomEventGenerator = RandomEventGenerator
+        
+    def get_current_time(self):
+        self.current_time = time.time() - self.start_time
+
+    def on_press(self, key):
+        try:
+            key_name = key.char
+        except AttributeError:
+            key_name = key.name
+
+        randkey_list = [                                                # 可以在这里改变randkey_list中的RandKey，从而修改通道，触发范围等等
+                        RandKey("A", "early", "medium", "short"),
+                        RandKey("A", "medium", "high", "medium"),
+                        RandKey("A", "medium", "extrem ", "long"),
+                        RandKey("both", "late", "extrem", "extrem"),
+                        RandKey("both", "last", "last", "last")
+        ]
+
+        if key_name == "q" or key_name == "Q" :
+            randkey = randkey_list[0]
+            unit = self.RandomEventGenerator.generate_unit_from_key(randkey)
+            self.PlayList.append(unit)
+        
+        elif key_name == "w" or key_name == "W" :
+            randkey = randkey_list[1]
+            unit =self.RandomEventGenerator.generate_unit_from_key(randkey)
+            self.PlayList.append(unit)
+            
+        elif key_name == "e" or key_name == "E" :
+            randkey = randkey_list[2]
+            unit = self.RandomEventGenerator.generate_unit_from_key(randkey)
+            self.PlayList.append(unit)
+        
+        elif key_name == "r" or key_name == "R" :
+            randkey = randkey_list[3]
+            unit = self.RandomEventGenerator.generate_unit_from_key(randkey)
+            self.PlayList.append(unit)
+        
+        elif key_name == "t" or key_name == "T" :
+            randkey = randkey_list[4]
+            unit = self.RandomEventGenerator.generate_unit_from_key(randkey)
+            self.PlayList.append(unit)
+
+        elif key_name == "o" or key_name == "O" or key_name == "enter" : # 30分钟项目，每5分钟提升一档
+            logger.warning("正在生成30分钟项目列表")
+            while self.PlayList.playtime < 1800:
+                if self.PlayList.playtime < 300:
+                    randkey = randkey_list[random.randint(0,2)]
+                    unit = self.RandomEventGenerator.generate_unit_from_key(randkey)
+                    self.PlayList.append(unit)
+                elif self.PlayList.playtime < 600:
+                    randkey = randkey_list[random.randint(1,3)]
+                    unit = self.RandomEventGenerator.generate_unit_from_key(randkey)
+                    self.PlayList.append(unit)
+                elif self.PlayList.playtime < 900:
+                    randkey = randkey_list[random.randint(2,4)]
+                    unit = self.RandomEventGenerator.generate_unit_from_key(randkey)
+                    self.PlayList.append(unit)
+                elif self.PlayList.playtime < 1200:
+                    randkey = randkey_list[random.randint(3,4)]
+                    unit = self.RandomEventGenerator.generate_unit_from_key(randkey)
+                    self.PlayList.append(unit)
+                else:
+                    randkey = randkey_list[4]
+                    unit = self.RandomEventGenerator.generate_unit_from_key(randkey)
+                    self.PlayList.append(unit)
+        
+        elif key_name == "p" or key_name == "P"  or key_name == "+" : # 60分钟项目，每400秒提升一档
+            logger.warning("正在生成60分钟项目列表")
+            while self.PlayList.playtime < 3600:
+                if self.PlayList.playtime < 400:
+                    randkey = randkey_list[random.randint(0,1)]
+                    unit = self.RandomEventGenerator.generate_unit_from_key(randkey)
+                    self.PlayList.append(unit)
+                elif self.PlayList.playtime < 800:
+                    randkey = randkey_list[random.randint(0,2)]
+                    unit = self.RandomEventGenerator.generate_unit_from_key(randkey)
+                    self.PlayList.append(unit)
+                elif self.PlayList.playtime < 1200:
+                    randkey = randkey_list[random.randint(1,3)]
+                    unit = self.RandomEventGenerator.generate_unit_from_key(randkey)
+                    self.PlayList.append(unit)
+                elif self.PlayList.playtime < 1600:
+                    randkey = randkey_list[random.randint(2,3)]
+                    unit = self.RandomEventGenerator.generate_unit_from_key(randkey)
+                    self.PlayList.append(unit)
+                elif self.PlayList.playtime < 2000:
+                    randkey = randkey_list[random.randint(1,4)]
+                    unit = self.RandomEventGenerator.generate_unit_from_key(randkey)
+                    self.PlayList.append(unit)
+                elif self.PlayList.playtime < 2400:
+                    randkey = randkey_list[random.randint(2,4)]
+                    unit = self.RandomEventGenerator.generate_unit_from_key(randkey)
+                    self.PlayList.append(unit)
+                elif self.PlayList.playtime < 2800:
+                    randkey = randkey_list[random.randint(3,4)]
+                    unit = self.RandomEventGenerator.generate_unit_from_key(randkey)
+                    self.PlayList.append(unit)
+                else:
+                    randkey = randkey_list[4]
+                    unit = self.RandomEventGenerator.generate_unit_from_key(randkey)
+                    self.PlayList.append(unit)
+        
+        elif key_name == "space":
+            self.PlayList.playtime = 0
+            logger.warning("已重置计时器，可随时添加时间项目")
+        
+        logger.info(f"已成功添加列表，当前列表前10个项目{self.PlayList.playlist_show[0:9]}，列表总长{len(self.PlayList.playlist_show)}")
+        
+        
+
+    def on_release(self, key):
+        try:
+            key_name = key.char
+        except AttributeError:
+            key_name = key.name
+        if key_name in self.key_pressed:
+            logger.debug(f"{key_name} key released")
+            del self.key_pressed[key_name]
+
+    async def run(self):
+        try:
+            while True:
+                await asyncio.sleep(0.1)
+        except KeyboardInterrupt:
+            pass
+
+class RandKey:
+    def __init__(self, channel, pattern, intensity, tick) -> None:
+        if channel in ["A", "B", "both"]:
+            self.channel = channel
+        else:
+            self.channel = "both"
+        if pattern in ["early", "medium", "late", "all", "last"]:
+            self.pattern = pattern
+        else:
+            self.pattern = "all"
+        if intensity in ["low", "medium", "high", "extrem", "last"]:
+            self.intensity = intensity
+        else:
+            self.intensity = "medium"
+        if tick in ["short", "medium", "long", "extrem", "last"]:
+            self.tick = tick
+        else:
+            self.tick = "medium"
+
+
+
+class RandomEventGenerator:
+    def __init__(self) -> None:
+        pass
+
+    def generate_unit_from_key(RandKey:RandKey):
+        if RandKey.channel == "both":
+            channel = "both"
+        else:
+            if random.uniform(0,1) < config["channel_rand"]: # 概率由单通道输出变双通道输出
+                channel = "both"
+            else:
+                channel = RandKey.channel
+        
+        pattern_randnum = random.randint(0,len(config["patterns_list"][RandKey.pattern]) - 1)
+        pattern = config["patterns_list"][RandKey.pattern][pattern_randnum]
+
+        intensity = random.randint(config["intensity_range"][RandKey.intensity][0], config["intensity_range"][RandKey.intensity][1])
+
+        tick = random.randint(config["rdm_tick_range"][RandKey.tick][0], config["rdm_tick_range"][RandKey.tick][1])
+
+        return PlayUnit(channel, pattern, intensity, tick)
+
+class PlayUnit:
+    def __init__(self, channel, pattern, intensity, tick) -> None:
+        self.channel = channel
+        self.pattern = pattern
+        self.intensity = intensity
+        self.tick = tick
+
+class PlayList:
+    def __init__(self, caoFanNiController) -> None:
+        self.caoFanNiController = caoFanNiController
+        self.playlist = []
+        self.playlist_show = []
+        self.playtime = 0
+        self.current_unit = None
+        self.current_play_start_time = 0
+        self.current_play_length = 0
+        
+    def append(self, playunit:PlayUnit):
+        self.playlist.append(playunit)
+        self.playlist_show.append((playunit.pattern, f"持续{playunit.tick / 10}秒", f"强度{playunit.intensity}%"))
+        self.playtime += playunit.tick / 10
+
+    def remove(self, playunit:PlayUnit):
+        self.playlist.remove(playunit)
+        self.playlist_show.remove((playunit.pattern, f"持续{playunit.tick / 10}秒", f"强度{playunit.intensity}%"))
+    
+    def play(self):
+        self.current_unit = self.playlist[0]
+        self.current_play_length = self.current_unit.tick / 10
+        self.current_play_start_time = time.time()
+        if self.current_unit.channel == "both":
+            asyncio.run_coroutine_threadsafe(
+                self.caoFanNiController.send_caoFanNi(self.current_unit.intensity,
+                                                    self.current_unit.tick, 
+                                                    self.current_unit.pattern, "A"),
+                self.caoFanNiController.loop
+            )
+            asyncio.run_coroutine_threadsafe(
+                self.caoFanNiController.send_caoFanNi(self.current_unit.intensity,
+                                                    self.current_unit.tick, 
+                                                    self.current_unit.pattern, "B"),
+                self.caoFanNiController.loop
+            )
+        else:
+            asyncio.run_coroutine_threadsafe(
+                self.caoFanNiController.send_caoFanNi(self.current_unit.intensity,
+                                                    self.current_unit.tick, 
+                                                    self.current_unit.pattern, self.current_unit.channel),
+                self.caoFanNiController.loop
+            )
+        logger.warning(f"当前输出{self.playlist_show[0]}")
+        self.remove(self.current_unit)
+        logger.info(f"当前等待列表前10个项目{self.playlist_show[0:9]}，列表总长{len(self.playlist_show)}")
+               
+    async def run(self):
+        try:
+            while True:
+                if len(self.playlist) > 0 and time.time() > self.current_play_start_time + self.current_play_length:
+                    self.play()
+                else:
+                    await asyncio.sleep(0.1)
+                
+        except KeyboardInterrupt:
+            pass
+
+
+class CaoFanNiController:
+    def __init__(self, websocket):
+        self.websocket = websocket
+        self.loop = asyncio.get_event_loop()
+
+    async def send_caoFanNi(self, i, ticks, pattern_name, channel="A"):
+        if i == 61:
+            print("inside")
+        event = asyncio.Event()
+        await self.caoFanNi(i, ticks, event, pattern_name, channel)
+        await event.wait()
+
+    async def caoFanNi(self, i, ticks, event, pattern_name, channel):
+        working_channel = channel
+        if not working_channel is None:
+            if working_channel == "A":
+                n_pattern = "A_pattern_name"
+                pattern_units = "A_pattern_units"
+                n_intensity = "A_intensity"
+                n_ticks = "A_ticks"
+            if working_channel == "B":
+                n_pattern = "B_pattern_name"
+                pattern_units = "B_pattern_units"
+                n_intensity = "B_intensity"
+                n_ticks = "B_ticks"
+
+            logger.warning(f"通道{working_channel} 强度{i} 持续{ticks/10}秒")
+            i = int(i * config["channel_level_factor"][working_channel])
+            if pattern_name in list(DIY_patterns.keys()):
+                await self.websocket.send(json.dumps({"cmd": "set_pattern", pattern_units: DIY_patterns[pattern_name],
+                                                n_intensity: i, n_ticks: ticks}))
+            else:
+                await self.websocket.send(json.dumps({"cmd": "set_pattern", n_pattern: pattern_name,
+                                                    n_intensity: i, n_ticks: ticks}))
+
+        await asyncio.sleep(ticks/10)
+        event.set()
+
+    async def get_max_intensive(self):
+        event = asyncio.Event()
+        await self.websocket.send(json.dumps({"cmd": "get_max_intensity"}))
+        max_int = await self.websocket.recv()
+        self.A_max, self.B_max = eval(max_int)["A_max"],  eval(max_int)["B_max"]
+        await event.wait()
+    
+    async def send_change_max(self, delta):
+        event = asyncio.Event()
+        await self.websocket.send(json.dumps({"cmd":  "change_max_intensity",  "delta_intensity":delta}))
+        event.set()
+
+async def main_fn(ws):
+    try:
+        logger.info(f"尝试连接至{ws}")
+        async with websockets.connect(ws) as websocket:
+            loop = asyncio.get_event_loop()
+            logger.success("Websocket连接成功")
+            caoFanNiController = CaoFanNiController(websocket)
+            # war_thunder = WarThunder(websocket, caoFanNiController, loop)
+            playlist = PlayList(caoFanNiController)
+            key_listener = KeyListener(loop, caoFanNiController, playlist)
+            # mouse_listener = MouseListener(websocket, caoFanNiController)
+            # await asyncio.gather(key_listener.run(), mouse_listener.run(), war_thunder.run())  ## War Thunder 没测试过，先注释了
+            await asyncio.gather(
+                key_listener.run(), 
+                playlist.run()
+                )
+    except websockets.exceptions.ConnectionClosed:
+        logger.error("WebSocket连接已关闭")
+    except Exception as e:
+        logger.error(f"发生错误：{e}")
+
+
+if __name__ == "__main__":
+    logger.remove()
+    handler_id = logger.add(sys.stderr, level=config["level"])
+    loop = asyncio.get_event_loop()
+    if loop.is_running():
+        logger.debug("警告：事件循环已经在运行中")
+    else:
+        loop.run_until_complete(main_fn(config["ws"]))
